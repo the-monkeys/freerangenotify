d306dcc07b7c402ae89a3f3ab5ccc4974530ab7c
Dave Augustus <devpandey19924u@gmail.com>
2026-03-20 14:23:47 +0530

    fix(ui): move logo to public dir for production builds

--- ui/src/components/ui/logo.tsx
+++ ui/src/components/ui/logo.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 const LogoWithName: React.FC = () => {
     return (
         <div className="flex items-center gap-2">
-            <img src="/src/public/logo/logo.svg" alt="FreeRangeNotify Logo" className="w-6 h-6" />
+            <img src="/logo/logo.svg" alt="FreeRangeNotify Logo" className="w-6 h-6" />
             <p className="text-base sm:text-lg font-semibold tracking-tight">FreeRange <span className="font-extralight">Notify</span></p>
         </div>
     );
@@ -11,7 +11,7 @@ const LogoWithName: React.FC = () => {
 
 export const Logo: React.FC<{ className?: string }> = ({ className }) => {
     return (
-        <img src="/src/public/logo/logo.svg" alt="FreeRangeNotify Logo" className={cn("w-6 h-6", className)} />
+        <img src="/logo/logo.svg" alt="FreeRangeNotify Logo" className={cn("w-6 h-6", className)} />
     );
 }
 
